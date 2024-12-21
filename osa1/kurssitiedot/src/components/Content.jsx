f8114b530074dc5a42480711eff682fd0b237e01
nooraso <noora.korhonen2@metropolia.fi>
2024-12-21 05:07:10 +0200

    1.3-1.5

--- osa1/kurssitiedot/src/components/Content.jsx
+++ osa1/kurssitiedot/src/components/Content.jsx
@@ -1,26 +1,21 @@
-const Part = (props) => {
-    return(
-        <p>
-            {props.name}: {props.exercises}
-        </p>
-    )
-}
+import Part from './Part'
 
 export const Content = (props) => {
     return(
         <div>
             <Part
-                name = {props.part1}
-                exercises = {props.exercises1}
+                name = {props.parts[0].name}
+                exercises = {props.parts[0].exercises}
             />
             <Part
-                name = {props.part2}
-                exercises = {props.exercises2}
+                name = {props.parts[1].name}
+                exercises = {props.parts[1].exercises}
             />
             <Part
-                name = {props.part3}
-                exercises = {props.exercises3}
+                name = {props.parts[2].name}
+                exercises = {props.parts[2].exercises}
             />
         </div>
     )
 }
+
